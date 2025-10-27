15aa28430537e1cd3481d80253226411c9efdd90
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 17:50:34 +0530

    Order imported

--- Happy_Meals/src/main.jsx
+++ Happy_Meals/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
 import About from './About.jsx'
-
+import Order from './Order.jsx'
 
 const root=createRoot(document.getElementById('root'));
 const path=window.location.pathname;
