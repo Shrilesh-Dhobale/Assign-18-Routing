1fd718f2decb488caaaac5213d397a873cf20fae
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 17:07:58 +0530

    About.css imported

--- Happy_Meals/src/About.jsx
+++ Happy_Meals/src/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './About.css'
 function About() {
   return (
-    <div>
-      
+    <div className='about-container'>
+      <h1 className='about-heading'>About Us</h1>
+      <p className='about-description'>We are dedicated to providing the best meal options for you.</p>
     </div>
   )
 }
