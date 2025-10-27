b804ec6e7ae9b8feef8c305a02a14b486ddcd679
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 18:15:06 +0530

    Navbar imported and called

--- Happy_Meals/src/Order.jsx
+++ Happy_Meals/src/Order.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Order.css'
+import Navbar from './Navbar.jsx'
 function Order() {
   return (
     <div className='order-container'>
+        <Navbar />
         <h1 className='order-heading'>Place Your Order</h1>
         <p className='order-description'>Choose from a variety of delicious meals and have them delivered to your doorstep.</p>
         <button className='order-button'>Order Now</button>
