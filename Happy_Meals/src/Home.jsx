61b58c55d391e520ce37546a6037837349a691e1
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 17:58:43 +0530

    Navbar component called and imported

--- Happy_Meals/src/Home.jsx
+++ Happy_Meals/src/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Home.css'
+import Navbar from './Navbar.jsx'
 function Home() {
   return (
     <div className='home-container'>
+        <Navbar />
       <h1 className='main-heading'>Welcome to Happy Meals</h1>
       <p className='subtitle'>Your one-stop solution for delicious meal options.</p>
     </div>
