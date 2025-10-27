bb9dcc7908e3f94b3cb04de8bfc963e194e86f6f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 17:19:39 +0530

    Home component imported

--- Happy_Meals/src/main.jsx
+++ Happy_Meals/src/main.jsx
@@ -1,10 +1,10 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import Home from './Home.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div>
+    <Home />
+  </div>
 )
