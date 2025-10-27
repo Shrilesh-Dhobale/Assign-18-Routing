f3b0bd8113c7df74ce8b7c5524e4449aed960f7d
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 17:57:03 +0530

    Navbar links added

--- Happy_Meals/src/Navbar.jsx
+++ Happy_Meals/src/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './Navbar.css'
 function Navbar() {
   return (
-    <div>
+    <div className='navbar'>
       
+      <nav className='navbar-links'>
+        <a href='/' className='navbar-link'>Home</a>
+        <a href='/about' className='navbar-link'>About</a>
+        <a href='/order' className='navbar-link'>Order</a>
+      </nav>
     </div>
   )
 }
