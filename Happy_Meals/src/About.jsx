6350fe6e91822f6fe6dbe6bf3f0e6c2b26e2f303
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 18:14:05 +0530

    Navbar imported and called

--- Happy_Meals/src/About.jsx
+++ Happy_Meals/src/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './About.css'
+import Navbar from './Navbar.jsx'
 function About() {
   return (
+    
     <div className='about-container'>
+      <Navbar />
       <h1 className='about-heading'>About Us</h1>
       <p className='about-description'>We are dedicated to providing the best meal options for you.</p>
     </div>
