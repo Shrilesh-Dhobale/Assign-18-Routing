5330365fb5abe40719214c7a964dde31ed762798
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-27 17:06:37 +0530

    About component created

--- Happy_Meals/src/Home.jsx
+++ Happy_Meals/src/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './Home.css'
 function Home() {
   return (
-    <div>
-      
+    <div className='home-container'>
+      <h1 className='main-heading'>Welcome to Happy Meals</h1>
+      <p className='subtitle'>Your one-stop solution for delicious meal options.</p>
     </div>
   )
 }
